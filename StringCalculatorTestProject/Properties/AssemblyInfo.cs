749e91801a2d746e2a351b8c9e68d5a07397ba67
STOREN Henrik <hstoren@one.steria.dom>
2018-10-02 15:15:41 +0200

    adding files

---
+++ StringCalculatorTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StringCalculatorTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SopraGroup")]
+[assembly: AssemblyProduct("StringCalculatorTestProject")]
+[assembly: AssemblyCopyright("Copyright © SopraGroup 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c2c383c6-dac2-4be9-a22f-9f4f892e5957")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StringCalculatorTestProject/StringCalculatorTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using StringCalculatorTDDWorkshop;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringCalculatorTestProject
+{
+    [TestClass]
+    public class StringCalculatorTests
+    {
+        [TestMethod]
+        public void CheckIfAddMethodExsists()
+        {
+            var calculator = new StringCalculator();
+            calculator.Add("");
+        }
+    }
+}
