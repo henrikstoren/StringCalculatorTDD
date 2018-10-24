37481c9937f0ba14b452fc0f2a15e4d5ff5fd10d
STOREN Henrik <hstoren@one.steria.dom>
2018-10-24 15:44:15 +0200

    Skrevet løsning

--- StringCalculatorTDDWorkshop/StringCalculator.cs
+++ StringCalculatorTDDWorkshop/StringCalculator.cs
@@ -8,5 +8,35 @@ namespace StringCalculatorTDDWorkshop
 {
     public class StringCalculator
     {
+        public int Add(string input)
+        {
+            var result = 0;
+            var separatorsToUse = new string[] { "," };
+            var numberPart = input;
+            if (!string.IsNullOrEmpty(input))
+            {
+                if (input.StartsWith("//"))
+                {
+                    var stringParts = input.Split('\n');
+
+                    var separatorPart = stringParts[0].Replace("/","");
+                    var separators = separatorPart.Substring(1, (separatorPart.Length - 2));
+                    separatorsToUse = separators.Split(new string[] { "][" }, StringSplitOptions.None);
+
+                    numberPart = stringParts[1];
+                }
+
+                var numbers = numberPart.Split(separatorsToUse, StringSplitOptions.None);
+                foreach (var num in numbers)
+                {
+                    var add = Int32.Parse(num);
+                    if(add < 1000)
+                    {
+                        result += add;
+                    }
+                }
+            }
+            return result;
+        }
     }
 }
--- StringCalculatorTestProject/StringCalculatorTests.cs
+++ StringCalculatorTestProject/StringCalculatorTests.cs
@@ -7,11 +7,78 @@ namespace StringCalculatorTestProject
     [TestClass]
     public class StringCalculatorTests
     {
+        StringCalculator calculator = new StringCalculator();
+
         [TestMethod]
         public void CheckIfAddMethodExsists()
         {
-            var calculator = new StringCalculator();
             calculator.Add("");
+        }
+
+        [TestMethod]
+        public void AddEmptyReturnsZero()
+        {
+            Assert.AreEqual(0, calculator.Add(""));
+        }
+
+        [TestMethod]
+        public void AddOneReturnsOne()
+        {
+            Assert.AreEqual(1, calculator.Add("1"));
+        }
+
+        [TestMethod]
+        public void AddTwoReturnsTwo()
+        {
+            Assert.AreEqual(2, calculator.Add("2"));
+        }
+
+        [TestMethod]
+        public void AddThreeReturnsThree()
+        {
+            Assert.AreEqual(3, calculator.Add("3"));
+        }
+
+        [TestMethod]
+        public void AddTwoNumbers()
+        {
+            Assert.AreEqual(3, calculator.Add("1,2"));
+        }
+
+        [TestMethod]
+        public void AddTwoNumbers2()
+        {
+            Assert.AreEqual(5, calculator.Add("1,4"));
+        }
+
+        [TestMethod]
+        public void AddThreeNumbers()
+        {
+            Assert.AreEqual(260, calculator.Add("1,25,234"));
+        }
+
+        [TestMethod]
+        public void AddGreaterThanThousandIgnoresIt()
+        {
+            Assert.AreEqual(342, calculator.Add("2,340,1005"));
+        }
+
+        [TestMethod]
+        public void SpesificSeparatorAdd()
+        {
+            Assert.AreEqual(17, calculator.Add("//[,,,]\n3,,,9,,,5"));
+        }
+
+        [TestMethod]
+        public void MultipleSeparatorAdd()
+        {
+            Assert.AreEqual(17, calculator.Add("//[%][*]\n3%9*5"));
+        }
+
+        [TestMethod]
+        public void MultipleSeparatorWithBracketsAdd()
+        {
+            Assert.AreEqual(19, calculator.Add("//[[]][*]\n3[]9*7"));
         }
     }
 }
